82202c1c06846ec4dbd32b827befa1f8aa131808
phyboro <phylisboro@gmail.com>
2023-04-23 14:19:52 -0700

    tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Set the config of a ssh_file
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+}
